737a7695b2666e06a96112b21c6097151abb1357
Mahabub Sunny <mahabubsany2@gmail.com>
2022-06-09 23:51:16 +0600

    FIx

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Next.js is Running with Custom Express</h1>
+      <Link href="/product/98?type=OK">Product Details</Link>
     </>
   );
 }
