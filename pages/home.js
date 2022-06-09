a8cc973831ac0e988c57a6642d7c52fd582d2837
Mahabub Sunny <mahabubsany2@gmail.com>
2022-06-09 23:46:40 +0600

    OK

---
+++ pages/home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Home - Next.js is Running with Custom Express</h1>
+    </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Next.js is Running with Custom Express</h1>
+    </>
+  );
+}
