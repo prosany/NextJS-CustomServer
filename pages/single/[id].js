1e258b6ffc51e098d47d00410d53718d3c065a33
Mahabub Sunny <mahabubsany2@gmail.com>
2022-06-10 22:35:48 +0600

    Add More to it

--- pages/single/[id].js
+++ pages/single/[id].js
@@ -1,24 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 const User = ({ user }) => {
   return (
-    <div className="row">
-      <div className="col-md-6 offset-md-3">
-        <div className="card">
-          <div className="card-body text-center">
-            Name: {user.name}
-            <br />
-            Email: {user.email}
-            <br />
-            Phone: {user.phone}
-            <br />
-            <small>Address: {user.address.city}</small> <br />
-            <Link href="/single">Go Back</Link>
+    <>
+      <Head>
+        <title>{user.name} - NextJS with Custom Express Server</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body text-center">
+              Name: {user.name}
+              <br />
+              Email: {user.email}
+              <br />
+              Phone: {user.phone}
+              <br />
+              <small>Address: {user.address.city}</small> <br />
+              <Link href="/single">Go Back</Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
