628be900b926ee89941b5c8a77d4a24a12d8399b
Mahabub Sunny <mahabubsany2@gmail.com>
2022-06-12 13:06:46 +0600

    Progress added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import "../styles/globals.css";
+import "../styles/nprogress.css";
+import Router from "next/router";
+import nProgress from "nprogress";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeComplete", nProgress.done);
+Router.events.on("routeChangeError", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
